6a39635b935b7e8f4fdaad71c5a3153b9b238db8
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-08 06:36:59 +0900

    1 (f) code done

--- a5/highway.py
+++ a5/highway.py
@@ -9,12 +9,20 @@ import torch
 import torch.nn as nn
 
 class Highway(nn.Module):
-    pass
     # Remember to delete the above 'pass' after your implementation
     ### YOUR CODE HERE for part 1f
 
+    def __init__(self, word_embed_size, init_gate_bias_mean=-1):
+        super().__init__()
+        self.proj = nn.Linear(word_embed_size, word_embed_size)
+        self.gate = nn.Linear(word_embed_size, word_embed_size)
+        self.gate.bias = nn.Parameter(self.gate.bias.data + init_gate_bias_mean)
 
-
+    def forward(self, x_conv_out):
+        x_proj = nn.functional.relu_(self.proj(x_conv_out))
+        x_gate = torch.sigmoid(self.gate(x_conv_out))
+        x_highway = x_gate * x_proj + (1 - x_gate) * x_conv_out
+        return x_highway
 
     ### END YOUR CODE
 
