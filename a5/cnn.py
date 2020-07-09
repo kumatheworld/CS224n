81c3bbfcbe1dc67dec596020ca0dd7b5eda98335
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-09 22:16:08 +0900

    1 (g) coding done

--- a5/cnn.py
+++ a5/cnn.py
@@ -9,12 +9,18 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    pass
     # Remember to delete the above 'pass' after your implementation
     ### YOUR CODE HERE for part 1g
 
+    def __init__(self, char_embedding_size, word_embed_size, kernel_size=5, padding=1):
+        super().__init__()
+        num_filters = word_embed_size
+        self.conv = nn.Conv1d(char_embedding_size, num_filters, kernel_size=kernel_size, padding=padding)
 
-
+    def forward(self, x_emb):
+        x_conv = self.conv(x_emb)
+        x_conv_out = torch.max(nn.functional.relu_(x_conv), dim=2).values
+        return x_conv_out
 
     ### END YOUR CODE
 
