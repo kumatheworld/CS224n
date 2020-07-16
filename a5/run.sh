2223a549307b8d71f31d9b2f6cc0c6febb6b9195
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-16 19:45:56 +0900

    Set batch size to 16 to avoid cuda error

--- a5/run.sh
+++ a5/run.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "train" ]; then
 	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./en_es_data/train.es --train-tgt=./en_es_data/train.en \
-        --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --cuda
+        --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --cuda --batch-size=16
 elif [ "$1" = "test" ]; then
     mkdir -p outputs
     touch outputs/test_outputs.txt
@@ -23,7 +23,7 @@ elif [ "$1" = "train_local_q2" ]; then
 elif [ "$1" = "test_local_q2" ]; then
     mkdir -p outputs
     touch outputs/test_outputs_local_q2.txt
-    python run.py decode model.bin ./en_es_data/test_tiny.es ./en_es_data/test_tiny.en outputs/test_outputs_local_q2.txt 
+    python run.py decode model.bin ./en_es_data/test_tiny.es ./en_es_data/test_tiny.en outputs/test_outputs_local_q2.txt
 elif [ "$1" = "vocab" ]; then
     python vocab.py --train-src=./en_es_data/train_tiny.es --train-tgt=./en_es_data/train_tiny.en \
         --size=200 --freq-cutoff=1 vocab_tiny_q1.json
